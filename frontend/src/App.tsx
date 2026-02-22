67bc9629e3770a33b2fae87032260b16ad019c4d
Soumik <smik1172@gmail.com>
2026-02-22 19:02:16 +0530

    feat: implemented webhook connection with backend to manage server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import ServerPage from "./components/ServerPage";
 import ImportServerPage from "./components/ImportServerPage";
-// import ServerDetailPage from "./components/ServerDetailPage";
+import ServerDetailPage from "./components/ServerDetailPage";
 import type { MinecraftServer } from "./types/server";
 
 function App() {
@@ -9,17 +9,17 @@ function App() {
         "servers" | "import" | "detail"
     >("servers");
     const [servers, setServers] = useState<MinecraftServer[]>([]);
-    // const [selectedServer, setSelectedServer] =
-    //     useState<MinecraftServer | null>(null);
+    const [selectedServer, setSelectedServer] =
+        useState<MinecraftServer | null>(null);
 
-    const handleImportServer = (serverData: MinecraftServer) => {        
-		setServers((prev) => [...prev, serverData]);
+    const handleImportServer = (serverData: MinecraftServer) => {
+        setServers((prev) => [...prev, serverData]);
         setCurrentPage("servers");
     };
 
     const handleServerSelect = (server: MinecraftServer) => {
-        // setSelectedServer(server);
-        // setCurrentPage("detail");
+        setSelectedServer(server);
+        setCurrentPage("detail");
     };
 
     return (
@@ -35,11 +35,11 @@ function App() {
                     onBack={() => setCurrentPage("servers")}
                     onImport={handleImportServer}
                 />
-            // : selectedServer ?
-            //     <ServerDetailPage
-            //         server={selectedServer}
-            //         onBack={() => setCurrentPage("servers")}
-            //     />
+            : currentPage === "detail" && selectedServer ?
+                <ServerDetailPage
+                    server={selectedServer}
+                    onBack={() => setCurrentPage("servers")}
+                />
             :   null}
         </>
     );
